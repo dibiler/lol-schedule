cdd31c78cec73ea9f8d6dd88d7b22b86dbdbfa07
dibiler <pleonors@gmail.com>
2023-02-20 15:48:27 +0100

    1 file modified

--- api.js
+++ api.js
@@ -1,23 +1,27 @@
 export class api {
-  constructor(){
-    this.url = 
+  headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
+    'Accept': '*/*',
+    'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://lolesports.com/',
+    'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
+    'Pragma': 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+    'Origin': 'https://esports-api.lolesports.com',
+    'Cache-Control': 'no-cache',
+    'TE': 'trailers'
+  }
+  url = 'https://cors-anywhere.herokuapp.com/https://esports-api.lolesports.com/persisted/gw/'
+  params = {hl: 'es-ES'};
+  async load(path,params){
+    let result = await fetch(this.url+path+'?'+ new URLSearchParams(...params,...this.params), {
+      method: "GET",
+      headers: new Headers(this.headers);
+    },
+    json = await result.json();
+
+    return json;
   }
 }
-/*fetch('https://cors-anywhere.herokuapp.com/https://esports-api.lolesports.com/persisted/gw/getSchedule?'+ new URLSearchParams({
-  hl: 'es-ES',
-    leagueId: '98767991302996019,100695891328981122,105266074488398661'
-}), {
-  method: "GET",
-  headers: new Headers({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
-'Accept': '*/*',
-'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
-'Accept-Encoding': 'gzip, deflate, br',
-'Referer': 'https://lolesports.com/',
-'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
-'Pragma': 'no-cache',
-'Access-Control-Allow-Origin': '*',
-'Origin': 'https://esports-api.lolesports.com',
-'Cache-Control': 'no-cache',
-'TE': 'trailers'})
-}).then(response => response.json()) 
-.then(json => console.log(json)); */
+
