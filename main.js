3cd7e315192093def2294673f2e3ac2b5c939c1b
dibiler <pleonors@gmail.com>
2023-02-23 11:33:22 +0100

    3 files modified

--- main.js
+++ main.js
@@ -12,25 +12,40 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card" id="leagues">
-    </div>
+      <div class="card" id="leagues">
+      </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `
-let leagues = await API.load('getLeagues')
-console.log(leagues)
+let leagues = await API.loadFake('getLeagues')
 if (leagues && leagues.data){
   var html=""
   leagues.data.leagues.forEach(league=>{
     html+=`
     <div>
       <a data-id="${league.id}">
-      <img src="${league.image}/>
+      <!--<img src="${league.image}"/>-->
       <span>${league.name}</span>
       </a>
     </div>
     `
+  })
+  async function refreshResults(){
+    let leagues = Array.from(document.getElementById("leagues").querySelectorAll('a.active')).map(item=>item.dataset.id)
+    if(leagues.length>0){
+      console.log(leagues)
+      let schedule = await API.loadFake('getSchedule',{leagueId:leagues.join(',')})
+      console.log(schedule)
+    }
+  }
+  document.getElementById("leagues").innerHTML = html
+  document.getElementById("leagues").addEventListener('click',e=>{
+    console.log(e.target)
+    if (e.target.nodeName=='A'){
+      e.target.classList.toggle('active')
+      refreshResults()
+    }
   })
 }
