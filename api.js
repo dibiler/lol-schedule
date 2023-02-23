bae9cf42ff99c042e2b1f74a3b8b0e41697d10d1
dibiler <pleonors@gmail.com>
2023-02-23 09:58:48 +0100

    2 files modified

--- api.js
+++ api.js
@@ -1,30 +1,26 @@
 export default class API {
-  
-  constructor(){
-    this.headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
-      'Accept': '*/*',
-      'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Referer': 'https://lolesports.com/',
-      'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
-      'Pragma': 'no-cache',
-      'Access-Control-Allow-Origin': '*',
-      'Origin': 'https://esports-api.lolesports.com',
-      'Cache-Control': 'no-cache',
-      'TE': 'trailers'
-    };
-    this.url = 'https://cors-anywhere.herokuapp.com/https://esports-api.lolesports.com/persisted/gw/';
-    this.params = {hl: 'es-ES'};
+  static headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
+    'Accept': '*/*',
+    'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://lolesports.com/',
+    'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
+    'Pragma': 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+    'Origin': 'https://esports-api.lolesports.com',
+    'Cache-Control': 'no-cache',
+    'TE': 'trailers'
   }
-  async function load(path,params){
-    let result = await fetch(this.url+path+'?'+ new URLSearchParams({...params,...this.params}), {
-      method: "GET",
-      headers: new Headers(this.headers)
-    }),
-    json = await result.json();
+  static url = 'https://cors-anywhere.herokuapp.com/https://esports-api.lolesports.com/persisted/gw/'
+  static params = {hl: 'es-ES'}
 
-    return json;
+  static async load(path,params){
+    let response  = await fetch(API.url+path+'?'+ new URLSearchParams({...params,...API.params}), {
+      method: "GET",
+      headers: new Headers(API.headers)
+    })
+    return await response.json()
   }
 }
 
--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
-import  {API}  from './api.js'
+import  API  from './api.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div class="card" id="leagues">
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
+let leagues = await API.load('getLeagues')
+console.log(leagues)
+if (leagues && leagues.data){
+  var html=""
+  leagues.data.leagues.forEach(league=>{
+    html+=`
+    <div>
+      <a data-id="${league.id}">
+      <img src="${league.image}/>
+      <span>${league.name}</span>
+      </a>
+    </div>
+    `
+  })
+}
